57fded502112f5cfcdf1a44dabfc3d8e418e0a80
Summer Wu <summer.wu82@gmail.com>
2016-02-14 07:53:03 -0800

    doors, avatar

---
+++ Assets/Scripts/WebinarDoor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebinarDoor : MonoBehaviour {
+
+	public bool open;
+	private float initx;
+	
+	// Use this for initialization
+	void Start () {
+		open = false;
+		initx = transform.position.x;
+	}
+	
+	// Update is called once per frame- threshhold for position
+	void Update () {
+		Debug.Log (initx);
+		if (open && transform.position.x > initx - 30) {
+			transform.Translate(Vector3.left * 10 * Time.deltaTime, Space.World);
+		}
+		
+		if (!open && transform.position.x < initx - 0.1) {
+			transform.Translate(Vector3.right * 10 * Time.deltaTime, Space.World);
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider collide){
+		if (collide.gameObject.tag == "Player") {
+			open = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider collide){
+		if (collide.gameObject.tag == "Player") {
+			open = false;
+		}
+	}
+}
